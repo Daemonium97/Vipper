70b52127853e56a66e3987cd79985ec3cc69d912
Daemonium97 <daemonium97@gmail.com>
2021-06-18 03:15:26 +0000

    adding features

---
+++ src/views/Software_Features.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container, Row } from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+
+export const Software_Features = () => {
+    return (
+       <Container fluid>
+            <Row>
+
+            </Row>
+       </Container>
+    )
+}
+
+
