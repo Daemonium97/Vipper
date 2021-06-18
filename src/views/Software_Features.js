23a1709affe76a1ea22144a3b742060ab1ad47d3
Daemonium97 <daemonium97@gmail.com>
2021-06-18 03:19:57 +0000

    saving

--- src/views/Software_Features.js
+++ src/views/Software_Features.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import { Container, Row } from 'react-bootstrap'
 import {Link} from 'react-router-dom'
+import call from '../features_images/callcenter.jpg'
+import horses from '../features_images/horses.jpg'
+import liveB from '../features_images/livebetting.jpg'
+import liveC from '../features_images/livecasino.jpg'
+import online from '../features_images/online-casino.jpg'
+import book from '../features_images/sportsbook.jpg'
 
 export const Software_Features = () => {
     return (
