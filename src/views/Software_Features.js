4041300f74749530e8875379d667d64f36883586
Daemonium97 <daemonium97@gmail.com>
2021-06-18 08:52:59 +0000

    saving

--- src/views/Software_Features.js
+++ src/views/Software_Features.js
@@ -34,7 +34,7 @@ export const Software_Features = () => {
 
                 <Col lg={true}>
                     <div className="fut1">
-                    <img src={book} alt="" id="ImageSports" />
+                        <img src={book} alt="" id="ImageSports" />
                     </div>
                 </Col>
                 <Col lg={true} >
@@ -71,22 +71,78 @@ export const Software_Features = () => {
             <Row>
                 <Col lg={true} className="text-center">
                     <div className="Sotext2">
-                        <p style={{color:'white'}}>VIP PER HEAD HAS ALL THE TOOLS THAT PROFESSIONAL BOOKIES</p>
-                        <p style={{color:'white'}}>NEED TO SUCCEED IN THEIR BUSINESS</p>
+                        <p style={{ color: 'white' }}>VIP PER HEAD HAS ALL THE TOOLS THAT PROFESSIONAL BOOKIES</p>
+                        <p style={{ color: 'white' }}>NEED TO SUCCEED IN THEIR BUSINESS</p>
                     </div>
 
                 </Col>
             </Row>
             <Row>
-                <Col lg={true}>
-                    <div className="square">
+                <Col lg={true} className="d-flex justify-content-center">
+                    <div className="square" style={{ background: '#190013' }}>
                         <div className="content">
+                            <ul style={{ listStyle: 'none' }}>
+                                <li style={{color:'#988c61'}}>Customized Sportsbook website </li>
+                                <li style={{color:'#988c61'}}>Online/Phone Services</li>
+                                <li style={{color:'#988c61'}}>Instant Activity Report</li>
+                                <li style={{color:'#988c61'}}>Customizable</li>
+                                <li style={{color:'#988c61'}}>Agent profile </li>
+                            </ul>
+
+                        </div >
+                    </div>
+                </Col>
+                <Col lg={true} className="d-flex justify-content-center">
+                    <div className="square" style={{ background: '#190013' }}>
+                        <div className="content">
+                            <ul style={{listStyle:'none', textAlign:'center', paddingRight:'7px'}}>
+                                <li style={{color:'#988c61'}}>Customizable Player Profile </li>
+                                <li style={{color:'#988c61'}}>Customizable limits</li>
+                                <li style={{color:'#988c61'}}>Master Account Management</li>
+                                <li style={{color:'#988c61'}}>Customizable Subagent Report
+</li>
+                                <li style={{color:'#988c61'}}>Management of Line Types
+</li>
+                            </ul>
 
                         </div>
                     </div>
                 </Col>
-                <Col lg={true}></Col>
-                <Col lg={true}></Col>
+                <Col lg={true} className="d-flex justify-content-center">
+                <div className="square" style={{ background: '#190013' }}>
+                        <div className="content">
+                            <ul style={{listStyle:'none'}}>
+                                <li style={{color:'#988c61'}}>Email Notification System</li>
+                                <li style={{color:'#988c61'}}>Limit all Services</li>
+                                <li style={{color:'#988c61'}}>Line Mover Option</li>
+                                
+                            </ul>
+
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col lg={true} style={{ display: 'flex' }} className="d-flex justify-content-center">
+                <div className="joinText">
+                    <h2>
+                        <div style={{ color: '#8f8753' }}>JOIN THE SELECT GROUP OF BOOKIES THAT USE VIP PER HEAD TODAY.</div>
+                        <div style={
+                            { textAlign: 'center', color: '#aea46a' }}>Complete the form and access a free 3-week trial.</div>
+                    </h2>
+                    <button class="custom-btn btn-5"
+                        style={{
+                            marginLeft: '490px',
+                            color: 'black',
+                            background: '#716a43',
+                            marginTop: '30px',
+                            fontSize: '15px',
+                            fontWeight: 'bold'
+
+
+                        }}><span>JOIN TODAY</span></button>
+                </div>
+                </Col>
             </Row>
 
         </Container>
