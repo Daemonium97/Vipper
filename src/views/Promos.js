bccb7f2b8712c5037cdb0e463a11239bbbb8b9fa
Daemonium97 <daemonium97@gmail.com>
2021-06-18 21:56:34 +0000

    adding promos

---
+++ src/views/Promos.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+import promo1 from '../promos_images/Promo1.jpg'
+import promo2 from '../promos_images/Promo2.jpg'
+import promo3 from '../promos_images/Promo3.jpg'
+import promo4 from '../promos_images/Promo4.jpg'
+import promo5 from '../promos_images/Promo5.jpg'
+
+
+export const Promos = () => {
+    return (
+        <Container fluid>
+            <Row style={{ background: 'black', textAlign: 'center', justifyContent: 'center' }}>
+                
+
+            </Row>
+        </Container>
+    )
+}
+
+
