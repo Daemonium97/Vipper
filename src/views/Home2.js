86a773e18b073f64b58ab6a940ce34cf2a17396a
Daemonium97 <daemonium97@gmail.com>
2021-06-19 08:23:36 +0000

    saving

--- src/views/Home2.js
+++ src/views/Home2.js
@@ -37,50 +37,113 @@ export const Home2 = () => {
                     <img src={image6} alt="" id="ImagePro" />
                 </Col>
             </Row>
-            <Row style={{ background: '#210019', justifyContent:'center', paddingBottom:'50px' }}>
+            <Row style={{ background: '#210019', justifyContent: 'center', paddingBottom: '50px' }}>
 
                 <div className="divHome1" style={{ textAlign: 'center', justifyContent: 'center', paddingTop: '50px' }}>
                     <h3 style={{ color: 'white', paddingBottom: '20px' }}>A PREMIUM EXPERIENCE</h3>
                     <h3 style={{ color: '#8e834d', paddingBottom: '50px' }}>FOR EVERYONE</h3>
                     <p style={{ color: 'white' }}>VIP Per Head is a company focused on providing the best Pay Per Head service on the market. We have designed a superior pay-per-head service for</p>
                     <p style={{ color: 'white' }}>professional bookies who demand the latest technologies, the best customer service, and the most comprehensive digital tools that will enable you to</p>
-                    <p style={{ color: 'white',paddingBottom:'40px' }}>manage your business anytime and anywhere.</p>
-                    <h4 style={{color:'white', fontSize:'40px'}}>THE BEST BETTING SERVICES</h4>
+                    <p style={{ color: 'white', paddingBottom: '40px' }}>manage your business anytime and anywhere.</p>
+                    <h4 style={{ color: 'white', fontSize: '40px' }}>THE BEST BETTING SERVICES</h4>
 
                 </div>
 
 
             </Row>
-            <Row style={{background: '#210019', justifyContent:'space-around'}}>
+            <Row style={{ background: '#210019', justifyContent: 'space-around' }}>
                 <Col lg={4}>
-                    <div style={{textAlign:'center'}}>
-                     <img src={book} alt="" id="ImagesMain"/> 
-                     <p style={{color:'white',fontSize:'24px'}}>Five-star experience, solid platform,</p>
-                        <p style={{color:'white',fontSize:'24px'}}>secure, and easy to use. Your customers</p>
-                        <p style={{color:'white',fontSize:'24px'}}>will be able to place bets online 24/7</p>  
+                    <div style={{ textAlign: 'center' }}>
+                        <img src={book} alt="" id="ImagesMain" />
+                        <p style={{ color: 'white', fontSize: '24px' }}>Five-star experience, solid platform,</p>
+                        <p style={{ color: 'white', fontSize: '24px' }}>secure, and easy to use. Your customers</p>
+                        <p style={{ color: 'white', fontSize: '24px' }}>will be able to place bets online 24/7</p>
                     </div>
                 </Col>
                 <Col lg={4}>
-                    <div style={{textAlign:'center'}}>
-                      <img src={liveC} alt="" id="ImagesMain"/>  
-                      <p style={{color:'white',fontSize:'24px'}}>Players can access our VIP game rooms</p>
-                        <p style={{color:'white',fontSize:'24px'}}>with first-rate dealers that will make sure</p>
-                        <p style={{color:'white',fontSize:'24px'}}>to meet all expectations.</p>
+                    <div style={{ textAlign: 'center' }}>
+                        <img src={liveC} alt="" id="ImagesMain" />
+                        <p style={{ color: 'white', fontSize: '24px' }}>Players can access our VIP game rooms</p>
+                        <p style={{ color: 'white', fontSize: '24px' }}>with first-rate dealers that will make sure</p>
+                        <p style={{ color: 'white', fontSize: '24px' }}>to meet all expectations.</p>
                     </div>
                 </Col>
                 <Col lg={4}>
-                    <div style={{textAlign:'center'}}>
+                    <div style={{ textAlign: 'center' }}>
                         <img src={online} alt="" id="ImagesMain" />
-                        <p style={{color:'white',fontSize:'24px'}}>Whether it’s on a phone, computer, or</p>
-                        <p style={{color:'white',fontSize:'24px'}}>tablet, we have a wide range of casino</p>
-                        <p style={{color:'white',fontSize:'24px'}}>games to choose from.</p>
+                        <p style={{ color: 'white', fontSize: '24px' }}>Whether it’s on a phone, computer, or</p>
+                        <p style={{ color: 'white', fontSize: '24px' }}>tablet, we have a wide range of casino</p>
+                        <p style={{ color: 'white', fontSize: '24px' }}>games to choose from.</p>
                     </div>
-                    
+
                 </Col>
-            
-            
-            
+
             </Row>
+
+            <Row style={{ background: '#210019', justifyContent: 'space-around', paddingTop: '40px' }}>
+                <Col lg={4}>
+                    <div style={{ textAlign: 'center' }}>
+                        <img src={liveB} alt="" id="ImagesMain" />
+                        <p style={{ color: 'white', fontSize: '24px' }}>Live betting available in all major leagues</p>
+                        <p style={{ color: 'white', fontSize: '24px' }}>and sports.</p>
+
+                    </div>
+                </Col>
+                <Col lg={4}>
+                    <div style={{ textAlign: 'center' }}>
+                        <img src={horses} alt="" id="ImagesMain" />
+                        <p style={{ color: 'white', fontSize: '24px' }}>Our racebook offers full features and the</p>
+                        <p style={{ color: 'white', fontSize: '24px' }}>availability to bet on the best tracks all</p>
+                        <p style={{ color: 'white', fontSize: '24px' }}>year round.</p>
+                    </div>
+                </Col>
+                <Col lg={4}>
+                    <div style={{ textAlign: 'center' }}>
+                        <img src={call} alt="" id="ImagesMain" />
+                        <p style={{ color: 'white', fontSize: '24px' }}>VIP pay per head customer service is</p>
+                        <p style={{ color: 'white', fontSize: '24px' }}>available 24/7/365 Phone, chat, and email. </p>
+
+                    </div>
+
+                </Col>
+
+            </Row>
+            <Row style={{ background: 'black', justifyContent: 'center', paddingTop: '80px' }}>
+                <div className="divHome2" style={{ textAlign: 'center' }}>
+                    <h7 style={{ color: '#72672b' }}>WHY VIP PER HEAD</h7>
+                    <p style={{ color: 'white', paddingTop: '70px' }}>VIP Per Head is a company focused on providing the best Pay Per Head service on the market. The VIP Per Head team has more than 20 years</p>
+                    <p style={{ color: 'white' }}>of experience in the industry. We know the needs of bookies and gamblers in depth. All this knowledge translates into a Pay Per Head service, whose</p>
+                    <p style={{ color: 'white', paddingBottom: '40px' }}>main Fobjective is to provide a premium experience for everyone.</p>
+                    <p style={{ color: 'white' }}>Our platform is a state-of-the-art white label bookmaking management solution built ourselves by our in-house team of experts.</p>
+
+                </div>
+            </Row>
+            <Row style={{ background: 'black', justifyContent: 'center', paddingTop: '80px' }}>
+                <Col lg={6}>
+                    <div className="divHome3" style={{ textAlign: 'center' }}>
+                        <h2 style={{ color: '#72672b',paddingTop:'50px' }}>THE BEST ALLY FOR YOUR</h2>
+                        <h2 style={{ color: '#72672b', paddingBottom:'40px' }}>BETTING BUSINESS</h2>
+                        <p style={{color:'white', padding:'0px 4px 30px 20px'}}>We have hired professional bookies to tailor VIP per head as a profitable and safe betting business.</p>
+                        <p style={{color:'white', padding:'0px 4px 30px 20px'}}>Vip Per Head tools and services allow bookies to create a business that operates 24 hours a day, seven days a week.</p>
+                        <p style={{color:'white', padding:'0px 4px 0px 20px'}}>The bookie will be able to delegate a large part of the operational task to the Vip Per Head team, which means that your business will continue to function even while you are asleep, and best of all, it will continue to generate income</p>
+
+                    </div>
+
+                </Col>
+                <Col lg={6}>
+                    <div className="divHome3" style={{ textAlign: 'center' }}>
+                        <h2 style={{ color: '#72672b',paddingTop:'50px' }}>LOW</h2>
+                        <h2 style={{ color: '#72672b', paddingBottom: '40px' }}>OPERATION COST</h2>
+                        <p style={{ color: 'white', paddingBottom:'60px' }}>Using VIP Per Head services is the best way to establish a fully functional betting business with large companies’ features at a low operating cost.</p>
+                        <p style={{ color: 'white', paddingBottom:'60px' }}>VIP Per Head charges a weekly fee per active player. Bookies will be able to use all the tools and services of VIP Per Head and will only have to pay for players who have placed a bet during the month.</p>
+                        <p style={{ color: 'white' }}>VIP Per Head charges a weekly fee per active player. Bookies will be able to use all the tools and services of VIP Per Head and will only have to pay for players who have placed a bet during the month.</p>
+                       
+
+
+                    </div>
+                </Col>
+            </Row>
+
 
         </Container>
     )
