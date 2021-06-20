01cb74a2068514654353fef58bcd7d40ae4bc0ef
Daemonium97 <daemonium97@gmail.com>
2021-06-20 16:09:48 +0000

    saving

--- src/components/Server.js
+++ src/components/Server.js
@@ -3,10 +3,11 @@ const app = express();
 const path = require('path');
 
 app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
 app.use(require('../routes/Route'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Form')));
 
 app.listen(3000, () =>{
     console.log('Server on port 3000')
